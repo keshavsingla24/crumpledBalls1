2dcc83e2206b7dfff27b72e14d7fb82e009d52d8
keshavsingla24 <71125512+keshavsingla24@users.noreply.github.com>
2020-10-31 23:30:57 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,20 @@
+class paper{
+    constructor(x,y,r){
+        var options = {
+            isStatic:false,
+            restitution:0.8,
+            friction:0.5,
+            density:1.2
+        }
+        this.r=r
+        this.body = Bodies.circle(x,y,this.r,options)
+        World.add(world,this.body)
+
+    }
+    display(){
+        var ppos = this.body.position
+        fill("yellow")
+        ellipseMode(RADIUS)
+        ellipse(ppos.x,ppos.y,this.r,this.r)
+    }
+}
